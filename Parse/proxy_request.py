f1b0297d33ab93a2f269699c5e2ceb1a2d64f0f2
Oleksandr Kamenskyi <okamenskyi@oleksmac.local>
2023-09-10 20:22:25 +0200

    Added Immoscout functionality. v1. Problem with captcha

--- Parse/proxy_request.py
+++ Parse/proxy_request.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 import requests
 import random
 
@@ -5,10 +6,26 @@ import random
 def proxy_request(url, **kwargs):
     ip = ["8.219.97.248:80"]
     ip_addresses = ["159.197.250.171:3128", "193.104.189.68:3128", "157.100.12.138:999", "8.219.97.248:80"]
+    AGENTS = [
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1309.0 Safari/537.17',
+        'Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0',
+        'Opera/12.80 (Windows NT 5.1; U; en) Presto/2.10.289 Version/12.02',
+        'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',
+        'Mozilla/3.0',
+        'Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3',
+        'Mozilla/5.0 (Linux; U; Android 0.5; en-us) AppleWebKit/522+ (KHTML, like Gecko) Safari/419.3',
+        'Opera/9.00 (Windows NT 5.1; U; en)'
+    ]
+    random_cookie = str(uuid4())
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        "Cookie": "reese84={}".format(random_cookie)
+    }
 
     try:
         # raise Exception
-        response = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+        response = requests.get(url, headers=headers)
         # self.logger.info("Request was made without proxies")
     except:
         # self.logger.info("Error. Looking for proxy....")
@@ -17,7 +34,7 @@ def proxy_request(url, **kwargs):
                 proxy = random.randint(0, len(ip_addresses) - 1)
                 proxies = {"http": ip_addresses[proxy], "https": ip_addresses[proxy]}
                 response = requests.get(url, proxies=proxies, timeout=5,
-                                        headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"},
+                                        headers=headers,
                                         **kwargs)
                 print(f"Proxy currently being used: {proxy}")
                 break
